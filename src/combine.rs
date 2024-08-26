6c02245dc0fcba5ccb79ce3cba9073546800f8eb
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-08-26 14:51:52 +0100

    removed commented older implementations

--- src/combine.rs
+++ src/combine.rs
@@ -51,39 +51,3 @@ mod tests {
         println!("Combined format result: {:?}", result.message);
     }
 }
-
-/*
-pub struct CombineFormat {
-    formats: Vec<BoxedLogFormat>,
-}
-
-impl CombineFormat {
-    pub fn new(formats: Vec<BoxedLogFormat>) -> Self {
-        Self { formats }
-    }
-}
-
-impl LogFormat for CombineFormat {
-    fn transform(&self, info: LogInfo, opts: Option<&HashMap<String, String>>) -> Option<LogInfo> {
-        let mut current_info = info;
-
-        for format in &self.formats {
-            if let Some(new_info) = format.transform(current_info, None) {
-                current_info = new_info;
-            } else {
-                return None;
-            }
-        }
-
-        Some(current_info)
-    }
-}
-/*
-pub fn combine(formats: Vec<Box<dyn LogFormat>>) -> CombineFormat {
-    CombineFormat::new(formats)
-}
-*/
-pub fn combine(formats: Vec<BoxedLogFormat>) -> BoxedLogFormat {
-    Box::new(CombineFormat::new(formats))
-}
-*/
