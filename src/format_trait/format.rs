f72bb415c69c5eb3a4550bb6ad946b1f4daa590b
ifeanyi-valentine <urielifeanyi@gmail.com>
2025-07-31 03:07:03 +0100

    refactor(format): restructure format trait and modules
    - move format trait to its own module
    - create a new format mod to re-export all format related modules

--- src/format_trait/format.rs
+++ src/format_trait/format.rs
@@ -1,21 +1,3 @@
-pub mod align;
-pub mod cli;
-pub mod colorize;
-pub mod json;
-pub mod label;
-pub mod logstash;
-mod macros;
-pub mod metadata;
-pub mod ms;
-pub mod pad_levels;
-pub mod pretty_print;
-pub mod printf;
-pub mod simple;
-pub mod timestamp;
-pub mod uncolorize;
-/* chaining of formats can be achieved by the `.chain` method on the `Format`
-instance hence the `combine` format is not needed  */
-
 pub trait Format {
     type Input;
 
